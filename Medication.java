764575aa437a3ca016387534f57425c709c2fd1a
Cíntia Dalila Soares <cintiasoares@gmail.com>
2023-07-09 12:18:42 -0400

    New class : Medication, for the array list of prescription

---
+++ Medication.java
@@ -0,0 +1,13 @@
+
+public class Medication {
+
+   private String name;
+   private int oneCycleDose;
+   private int numberOfRep;
+
+   public Medication (String name, int oneCycleDose, int numberOfRep){
+    this.name = name;
+    this.oneCycleDose = oneCycleDose;
+    this.numberOfRep = numberOfRep;
+   }
+}
